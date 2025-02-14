4f6ddcc96c85d6effa24bdc7f7b3e8d60fcc54e3
Stephane <stephanevale336@gmail.com>
2025-02-14 16:22:25 -0300

    Primeiro commit

---
+++ rodape.php
@@ -0,0 +1,6 @@
+</div>
+<script type="text/javascript" src="./resources/lib/jQuery/js/jquery-3.1.1.js"></script>
+<script type="text/javascript" src="./resources/lib/bootstrap/js/bootstrap.js"></script>
+<script type="text/javascript" src="./resources/common/js/crud.js"></script>
+</body>
+</html>
